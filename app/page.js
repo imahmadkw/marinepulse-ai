236fc482202787ed3de8584f4773dcc8f3acb54b
imahmadkw <ahmadmoneef197@gmail.com>
2026-05-27 02:46:35 +0300

    page.js

---
+++ app/page.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main style={{
+      background: "#07111f",
+      color: "white",
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      fontFamily: "Arial"
+    }}>
+      <h1>🌊 MarinePulse AI</h1>
+      <p>Marine Trend Prediction Platform</p>
+    </main>
+  )
+}
